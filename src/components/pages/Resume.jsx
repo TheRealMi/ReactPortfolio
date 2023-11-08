83f11fd2e0071931ad8ec10cf4d6e6a655ab6985
The Real Mi <miaolmedillo@gmail.com>
2023-11-08 10:57:20 -0500

    added resume page

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,9 +1,39 @@
 export default function Resume() {
   return (
     <section className="contentSection">
-        <section id="contact" className="sectionTitle">
-            <h2>My Resume</h2>
-        </section>
+      <section id="contact" className="sectionTitle">
+        <h2>My Resume</h2>
+      </section>
+      <section className="text">
+        <h4>
+          <a href="./assets/images/Mia Olmedillo resume update.pdf">
+            Download my Resume!
+          </a>
+        </h4>
+        <br />
+        <br />
+        <h4>My Proficiencies</h4>
+        <br />
+        <ul>
+          <li>HTML/CSS</li>
+          <li>Bootstrap</li>
+          <li>JavaScript</li>
+          <li>Node</li>
+          <li>MySQL</li>
+          <li>MongoDB</li>
+          <li>React</li>
+        </ul>
+        <h4>My Skills</h4>
+        <br />
+        <ul>
+          <li>Creative Problem Solving</li>
+          <li>Data Entry</li>
+          <li>Attention to Detail</li>
+          <li>Teamwork</li>
+          <li>Collaborative Leadership</li>
+          <li>Time Management</li>
+        </ul>
+      </section>
     </section>
   );
 }
