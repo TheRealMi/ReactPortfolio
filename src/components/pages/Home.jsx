6339370cd4aec05b70381805ffce79988099d29c
The Real Mi <miaolmedillo@gmail.com>
2024-01-08 14:41:43 -0500

    added arrow to menu

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,10 +4,20 @@ export default function Home() {
     return (
         <div>
             <img className="menuTitle" src="/assets/images/codedByMi.png" alt="coded by Mi" />
-            <button className='menuButtons'><img src="/assets/images/aboutMi.png" alt="about Mi" /></button>
-            <button className='menuButtons'><img src="/assets/images/Projects.png" alt="Projects" /></button>
-            <button className='menuButtons'><img src="/assets/images/resume.png" alt="resume" /></button>
-            <button className='menuButtons'><img src="/assets/images/contactMi.png" alt="contact Mi" /></button>
+            <div className="menuContainer">
+            <button className='menuButtons'>
+                <img className="menuArrow" src="/assets/images/menuArrow.png" alt="menu arrow" />
+                <img src="/assets/images/aboutMi.png" alt="about Mi" /></button>
+            <button className='menuButtons'>
+                <img className="menuArrow" src="/assets/images/menuArrow.png" alt="menu arrow" />
+                <img src="/assets/images/Projects.png" alt="Projects" /></button>
+            <button className='menuButtons'>
+                <img className="menuArrow" src="/assets/images/menuArrow.png" alt="menu arrow" />
+                <img src="/assets/images/resume.png" alt="resume" /></button>
+            <button className='menuButtons'>
+                <img className="menuArrow" src="/assets/images/menuArrow.png" alt="menu arrow" />
+                <img src="/assets/images/contactMi.png" alt="contact Mi" /></button>
+            </div>
         </div>
     );
 }
