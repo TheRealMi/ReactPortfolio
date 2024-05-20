1e94bd7d1d227f355899e0ad5b0d63cc7b81c641
The Real Mi <miaolmedillo@gmail.com>
2024-05-20 17:05:12 -0400

    worked on resume page

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -2,18 +2,23 @@ import classes from './resume.module.css';
 
 export default function Resume() {
   return (
-    <section className="contentSection">
-      <section>
-      <section className={classes.container}>
+     <section className="contentSection">
+      <div className={classes.container}>
         <img src="./assets/images/ResumeScroll.png" alt="Pixelated Scroll"/>
           <a className={classes.scrollText} href="./assets/images/Mia Olmedillo resume update.pdf">
             Download my Resume!
           </a>
-      </section>
-      </section>
-      <section className="text">
-        <h4>My Proficiencies</h4>
-        <br />
+      </div>
+      <div className={classes.container}>
+      <h4>Transferable Skills</h4>
+      <div className={classes.icons}>
+        
+      </div>
+      </div>
+      <div className="text">
+        <h4>Professional Summary</h4>
+        <p>Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <h4>Technical Skills</h4>
         <ul>
           <li>HTML/CSS</li>
           <li>Bootstrap</li>
@@ -23,17 +28,8 @@ export default function Resume() {
           <li>MongoDB</li>
           <li>React</li>
         </ul>
-        <h4>My Skills</h4>
-        <br />
-        <ul>
-          <li>Creative Problem Solving</li>
-          <li>Data Entry</li>
-          <li>Attention to Detail</li>
-          <li>Teamwork</li>
-          <li>Collaborative Leadership</li>
-          <li>Time Management</li>
-        </ul>
-      </section>
+        
+      </div>
     </section>
   );
 }
