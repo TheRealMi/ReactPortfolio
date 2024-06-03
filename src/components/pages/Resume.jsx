a9ccf781769d7efeb4e39f4b2c131d85140d5f68
The Real Mi <miaolmedillo@gmail.com>
2024-06-03 12:38:43 -0400

    added icons to technical skills

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,4 +1,7 @@
 import classes from './resume.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDatabase } from '@fortawesome/free-solid-svg-icons'
+import { faHtml5, faCss3Alt, faBootstrap, faJs, faNode, faReact, faEnvira} from '@fortawesome/free-brands-svg-icons';
 
 export default function Resume() {
   return (
@@ -14,17 +17,24 @@ export default function Resume() {
         <p>Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         <h4>Technical Skills</h4>
         <ul>
+          <FontAwesomeIcon icon={faHtml5} size="5x"/>
           <li>HTML/CSS</li>
+          <FontAwesomeIcon icon={faCss3Alt} size="5x"/>
           <li>Bootstrap</li>
+          <FontAwesomeIcon icon={faBootstrap} size="5x"/>
           <li>JavaScript</li>
+          <FontAwesomeIcon icon={faJs} size="5x"/>
           <li>Node</li>
+          <FontAwesomeIcon icon={faNode} size="5x"/>
           <li>MySQL</li>
+          <FontAwesomeIcon icon={faDatabase} size="5x"/>
           <li>MongoDB</li>
+          <FontAwesomeIcon icon={faEnvira} size="5x"/>
           <li>React</li>
+          <FontAwesomeIcon icon={faReact} size="5x"/>
         </ul>
-        
       </div>
-      <div className={classes.container}>
+      <div className={classes.skills}>
       <h4>Transferable Skills</h4>
       <div className={classes.icongrid}>
         <img src="https://picsum.photos/100/100" alt="" />
